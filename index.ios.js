48b6f89836cb0decfee9050d14c5cedbb923d99f
hongjie <32968210@qq.com>
2017-12-07 13:20:04 +0800

    初始化项目

--- index.ios.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-import { AppRegistry } from 'react-native';
-import Tomato from './src';
-
-AppRegistry.registerComponent('Tomato', () => Tomato);
--- src/config.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-export const UPDATE_HOST = 'http://192.168.56.1';
-
-// talkingData appID: CAF0C7D3D33A4FB8B9EEA580503130BC
